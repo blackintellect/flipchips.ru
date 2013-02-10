1506040512c876480164fd6c66b06b6f56aa0c70
Igor <Igor@insane>
2013-02-10 22:27:51 +0400

    Add files: init.php and test_connection.php

--- init/init.php
+++ init/init.php
@@ -1,4 +1,28 @@
 <?php
-function __autoload() {
-	
+
+/**
+ * Array of constants
+ */
+$C = array();
+
+// add in C some MYSQL consts
+$C['HOST'] = 'localhost';
+$C['DATABASE'] = 'flipchips.ru';
+$C['USER'] = 'root';
+$C['PASSWORD'] = '';
+
+foreach ($C as $name => $value) {
+	define($name, $value);
+}
+
+/**
+ * __autoload
+ *  
+ * Function evaluates then undefined class find
+ * Search in ../class/*  
+ * 
+ * @param string $class_name
+ */
+function __autoload($class_name) {
+	include_once '../class/' . $class_name . 'php';	
 }
